c28a9870844e8283f010bb74e2ed23f8bf723f7e
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2023-01-16 23:01:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,59 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
-const App = () => {
-  return (
-    <></>
-  )
-}
-
-
-export default App;
+import React, { Component, useState } from "react";
+ import "../styles/App.css";
+ 
+ class App extends React.Component {
+   constructor() {
+     super();
+     this.state = {
+       currentSlide: 0,
+     };
+   }
+ 
+   render() {
+     return (
+       <>
+         <h1 data-testid="title">
+           {this.props.slides[this.state.currentSlide].title}
+         </h1>
+         <p data-testid="text">
+           {this.props.slides[this.state.currentSlide].text}
+         </p>
+         <button
+           data-testid="button-prev"
+           disabled={this.state.currentSlide === 0}
+           onClick={() => {
+             this.setState({
+               currentSlide: this.state.currentSlide - 1,
+             });
+           }}
+         >
+           Previous
+         </button>
+         <button
+           data-testid="button-next"
+           disabled={this.state.currentSlide === this.props.slides.length - 1}
+           onClick={() => {
+             this.setState({
+               currentSlide: this.state.currentSlide + 1,
+             });
+           }}
+         >
+           Next
+         </button>
+         <button
+           onClick={() => {
+             this.setState({
+               currentSlide: 0,
+             });
+           }}
+           disabled={this.state.currentSlide===0}
+           data-testid="button-restart"
+         >
+           Restart
+         </button>
+       </>
+     );
+   }
+ }
+ 
+ export default App;
